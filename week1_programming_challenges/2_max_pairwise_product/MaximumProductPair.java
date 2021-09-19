929080b2c96c726bc0621b61d3a58163bd14dffc
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-09-19 17:36:25 +0530

    Added stress test for MaximumProductPair

--- week1_programming_challenges/2_max_pairwise_product/MaximumProductPair.java
+++ week1_programming_challenges/2_max_pairwise_product/MaximumProductPair.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Random;
 
 public class MaximumProductPair {
 	public static void main(String[] args) {
@@ -10,6 +11,7 @@ public class MaximumProductPair {
 		}
 		scanner.close();
 		System.out.println(calculateMaxProduct(numbers));
+		// stressTest(15, 20);
 	}
 
 	private static long calculateMaxProduct(long[] numbers) {
@@ -23,5 +25,34 @@ public class MaximumProductPair {
 			}
 		}
 		return max * nextMax;
+	}
+
+	private static long calculateMaxProductBruteforce(long[] numbers) {
+		long max = 0;
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				long product = numbers[i] * numbers[j];
+				max = product > max ? product : max;
+			}
+		}
+		return max;
+	}
+
+	private static void stressTest(int maxElements, int maxValue) {
+		Random random = new Random();
+		while (true) {
+			int n = random.nextInt(maxElements);
+			long[] numbers = new long[n];
+			for (int i = 0; i < n; i++) {
+				int value = random.nextInt(maxValue);
+				System.out.println(value);
+				numbers[i] = value;
+			}
+			System.out.println(Arrays.toString(numbers));
+			if (calculateMaxProductBruteforce(numbers) != calculateMaxProduct(numbers)) {
+				System.out.println("FAIL: " + Arrays.toString(numbers));
+				break;
+			}
+		}
 	}
 }
